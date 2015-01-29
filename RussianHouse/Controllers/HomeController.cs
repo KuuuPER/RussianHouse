84efbe37e5463dd35182e60746a8126c7cffdf32
KuuuPER <a.kouprienko@corvusalba.ru>
2015-01-29 08:16:00 +0300

    design changes and added http listener

---
+++ RussianHouse/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RussianHouse.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult AboutProject()
+        {
+            ViewBag.Message = "Опроекте";
+
+            return View();
+        }
+
+        public ActionResult FundRising()
+        {
+            ViewBag.Message = "Сбор средств";
+
+            return View();
+        }
+
+        public ActionResult News()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Страница контактов";
+
+            return View();
+        }
+
+        public ActionResult Partners()
+        {
+            ViewBag.Message = "Наши партнёры";
+
+            return View();
+        }
+
+        public ActionResult Team()
+        {
+            ViewBag.Message = "Наша команда";
+
+            return View();
+        }
+    }
+}
---
+++ RussianHouse/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RussianHouse")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RussianHouse")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ecb58dfc-30c4-4a46-a078-c87195b9270c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
